6c86a446f1d9c9ee7b60e18021bd1d2de31f7fce
“HendMohammed90” <hend.odesk.tasks@gmail.com>
2020-10-27 13:17:55 +0200

    Adjust cart module

--- cart.js
+++ cart.js
@@ -1,9 +1,21 @@
-const Product  = require('./product');
-module.exports =  class Cart extends Product{
+// const Product  = require('./product');
+// module.exports =  class Cart extends Product{
 
-    constructor(userId ,products ){
-        super();
-        this.products = [Product];
+//     constructor(userId ,products ){
+//         super();
+//         this.products = [Product];
+//         this.userId = userId ;
+
+//     }
+//     addProduct(){ 
+//         console.log('Product added');
+//     }
+// }
+
+module.exports =  class Cart{
+
+    constructor(userId , products ){
+        this.products = products;
         this.userId = userId ;
 
     }
