dc537244e208ff463fb6223814cf4dc61553adb5
“HendMohammed90” <hend.odesk.tasks@gmail.com>
2020-11-18 12:53:33 +0200

    Adjust Product file

--- product.js
+++ product.js
@@ -1,21 +1,35 @@
-products = [];
-class Product{
+// products = [];
+// class Product{
 
-    constructor(name ,amount , price){
+//     constructor(name ,amount , price){
+//         this.name = name ;
+//         this.amount = amount ;
+//         this.price = price ;
+//         // this.products = [];
+//     }
+
+//     add(product){
+//         // products.push(product)
+//         products.push(product);
+//         // this.products =[...this.products, ...product];
+//         // console.log(products);
+//         // return (this.products);
+//         // console.log(this);
+//     }
+// }
+// exports.products = products ;
+// exports.Product = Product ;
+
+
+module.exports =  class Product{
+
+    constructor(name ,amount , price ){
         this.name = name ;
         this.amount = amount ;
         this.price = price ;
-        // this.products = [];
     }
 
-    add(product){
-        // products.push(product)
-        products.push(product);
-        // this.products =[...this.products, ...product];
-        // console.log(products);
-        // return (this.products);
+    add(){
+        console.log("Product Add to the Database ^_^");
     }
 }
-exports.products = products ;
-exports.Product = Product ;
-
